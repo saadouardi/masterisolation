8d5003ebd10b84f15c91030bcde7bb0388db27af
SaadOuardi <saad.ouardi.developer@gmail.com>
2025-06-05 15:10:22 +0200

    v6.5.25.3.10

--- frontend/src/components/Articals/Articals.jsx
+++ frontend/src/components/Articals/Articals.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Title } from '../../components/Common/Title';
+import Artical1 from '../../assets/images/slider/1.jpg';
+import Artical2 from '../../assets/images/slider/2.webp';
+import Artical3 from '../../assets/images/slider/3.jpg';
 import './Artical.scss';
 
 export const Articals = () => {
+    const articals = [
+        { id: 1, image: Artical1, title: 'Artical 1' },
+        { id: 2, image: Artical2, title: 'Artical 2' },
+        { id: 3, image: Artical3, title: 'Artical 3' }
+    ];
+    
     return (
         <section>
             <Title title='CHECK OUR ARTICALS'/>
             <div className="articals">
-                <div className="artical artical-1">
-                    <button>READ MORE</button>
-                </div>
-
-                <div className="artical artical-2">
-                    <button>READ MORE</button>
-                </div>
-
-                <div className="artical artical-3">
-                    <button>READ MORE</button>
-                </div>
+                {articals.map(artical => (
+                    <div 
+                        className="artical" 
+                        key={artical.id}
+                        style={{ backgroundImage: `url(${artical.image})` }}
+                    >
+                        <button>READ MORE</button>
+                    </div>
+                ))}
             </div>
         </section>
     )
